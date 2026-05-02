b9fb3654a895c89e5eca247d1fb26a8541da84d7
Aleksandar Bursac <aleksbrsc@gmail.com>
2026-05-02 17:23:33 -0400

    feat(frontend): home and index styles rework

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import NamePicker from '../components/NamePicker'
 // import ShareLink from '../components/ShareLink'
+import homeArt from '../assets/images/home_art.webp'
 import './HomePage.css'
 
 export default function HomePage() {
@@ -10,6 +11,7 @@ export default function HomePage() {
 
   return (
     <div className="home">
+      <div className="home__sidebar" />
       <main className="home__content">
         <div className="home__logo-row">
           <h1 className="home__title">Kongroo</h1>
@@ -37,9 +39,9 @@ export default function HomePage() {
             <NamePicker value={authorName} onChange={setAuthorName} />
           </div>
 
-          <div className="home__cta-wrapper">
+          <div className="home__start-wrapper">
           <button
-            className="home__cta"
+            className="home__start"
             disabled={!authorName}
             onClick={() => navigate('/lobby', { state: { authorName } })}
           >
@@ -54,13 +56,13 @@ export default function HomePage() {
         <div className="home__join-section">
           <p className="home__join-hint">See existing lobbies!</p>
           <div className="home__join-wrapper">
-            <button className="home__join" onClick={() => navigate('/lobby')}>
+            <button className="home__join" disabled={!authorName} onClick={() => navigate('/lobby')}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                 <g clipPath="url(#clip0_5_109)">
-                  <path d="M1.5 6.5C1.5 3.46243 3.96243 1 7 1C10.0376 1 12.5 3.46243 12.5 6.5C12.5 9.53757 10.0376 12 7 12C3.96243 12 1.5 9.53757 1.5 6.5Z" fill="#620437"/>
-                  <path d="M14.4999 6.5C14.4999 8.00034 14.0593 9.39779 13.3005 10.57C14.2774 11.4585 15.5754 12 16.9999 12C20.0375 12 22.4999 9.53757 22.4999 6.5C22.4999 3.46243 20.0375 1 16.9999 1C15.5754 1 14.2774 1.54153 13.3005 2.42996C14.0593 3.60221 14.4999 4.99966 14.4999 6.5Z" fill="#620437"/>
-                  <path d="M0 18C0 15.7909 1.79086 14 4 14H10C12.2091 14 14 15.7909 14 18V22C14 22.5523 13.5523 23 13 23H1C0.447716 23 0 22.5523 0 22V18Z" fill="#620437"/>
-                  <path d="M16 18V23H23C23.5522 23 24 22.5523 24 22V18C24 15.7909 22.2091 14 20 14H14.4722C15.4222 15.0615 16 16.4633 16 18Z" fill="#620437"/>
+                  <path d="M1.5 6.5C1.5 3.46243 3.96243 1 7 1C10.0376 1 12.5 3.46243 12.5 6.5C12.5 9.53757 10.0376 12 7 12C3.96243 12 1.5 9.53757 1.5 6.5Z"/>
+                  <path d="M14.4999 6.5C14.4999 8.00034 14.0593 9.39779 13.3005 10.57C14.2774 11.4585 15.5754 12 16.9999 12C20.0375 12 22.4999 9.53757 22.4999 6.5C22.4999 3.46243 20.0375 1 16.9999 1C15.5754 1 14.2774 1.54153 13.3005 2.42996C14.0593 3.60221 14.4999 4.99966 14.4999 6.5Z"/>
+                  <path d="M0 18C0 15.7909 1.79086 14 4 14H10C12.2091 14 14 15.7909 14 18V22C14 22.5523 13.5523 23 13 23H1C0.447716 23 0 22.5523 0 22V18Z"/>
+                  <path d="M16 18V23H23C23.5522 23 24 22.5523 24 22V18C24 15.7909 22.2091 14 20 14H14.4722C15.4222 15.0615 16 16.4633 16 18Z"/>
                 </g>
                 <defs>
                   <clipPath id="clip0_5_109">
@@ -73,6 +75,7 @@ export default function HomePage() {
           </div>
         </div>
       </main>
+      <img src={homeArt} alt="" className="home__hero-art" />
     </div>
   )
 }
